60c78c7d40dbc7a36f691baec0c94162dbb63a81
Nyasita <72727098+Nyasita@users.noreply.github.com>
2020-12-11 10:37:00 +0300

    Update group1assignment.sh

--- group1assignment.sh
+++ group1assignment.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-#Create a new project, complete with all directories
+#Usage: mkdir - to make new directories, wget - to download sequences, awk and grep - for pattern searching, wc - for counting
+
+
+#QN1:Create a new project, complete with all directories
 mkdir group1assignment
 cd group1assignment
 mkdir bashcommands
 cd bashcommands
 
-#download and uncompress the data into the appropriate directory
+#QN2:download and uncompress the data into the appropriate directory
 wget https://github.com/kipkurui/Intro2Linux2019/raw/master/Data/protein.fa.gz
 gunzip protein.fa.gz
 
-#create a new FASTA file which only contains uncharacterized proteins
+#QN3:create a new FASTA file which only contains uncharacterized proteins
 awk '{RS=">";FS="\n"}; {if ($1~/uncharacterized/) print RS$0}' protein.fa > uncharacterized.fa
 
-#create a clean FASTA file without uncharacterized sequences
-grep -v "uncharacterized" protein.fa > clean.fa  
+#QN4:create a clean FASTA file without uncharacterized sequences
+grep -v "uncharacterized" protein.fa > clean.fa  #alternatively the command awk '{RS=""}{gsub(/\n/," ", $0) print $0}' can be used for this question
 
-#Create a new file which only contains raw sequences without the identifying information, or new lines
+#QN5:create a new file which only contains raw sequences without the identifying information, or new lines
 grep -v ">" protein.fa | tr -d "\n" > oneline.fa 
 
-#Number of amino acids are in this new file
+#QN6:Number of amino acids are in this new file
 wc -c oneline.fa
+
